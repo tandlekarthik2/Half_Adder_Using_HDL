e4e731e03384e42f861224a3f09cb54629c48926
tandlekarthik2 <tandlekarthik2@gmail.com>
2025-09-07 20:03:13 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 
-module tt_um_example (
+module tt_um_karthik_halfadder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
